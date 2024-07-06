d86638a8495e0d1b9bb7908908ab0279603b3bf7
Stanislav Orlov <stanislav.orlov@ukma.edu.ua>
2024-07-06 11:10:37 +0300

    Basic navigation settings

--- app/src/main/java/com/example/transactionsapp/ui/TransactionsApp.kt
+++ app/src/main/java/com/example/transactionsapp/ui/TransactionsApp.kt
@@ -1,14 +1,59 @@
 package com.example.transactionsapp.ui
 
+import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.transactionsapp.R
+import com.example.transactionsapp.data.Category
+import com.example.transactionsapp.ui.theme.TransactionsAppTheme
+
+enum class TransactionScreen {
+    Main, AddTransaction
+}
 
 @Composable
 fun TransactionsApp(
     viewModel: TransactionsViewModel = viewModel(),
     navController: NavHostController = rememberNavController(),
 ) {
+    NavHost(navController = navController, startDestination = TransactionScreen.Main.name) {
+        composable(TransactionScreen.Main.name) {
+
+        }
+
+        composable(TransactionScreen.AddTransaction.name) {
+
+        }
+    }
 
 }
