8697f778012afba8ccbf13e14af990328c788f4e
Stanislav Orlov <stanislav.orlov@ukma.edu.ua>
2024-07-09 23:27:24 +0300

    Implemented paging

--- app/src/main/java/com/example/transactionsapp/data/TransactionsRepository.kt
+++ app/src/main/java/com/example/transactionsapp/data/TransactionsRepository.kt
@@ -1,16 +1,15 @@
 package com.example.transactionsapp.data
 
 
+import androidx.paging.PagingSource
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 interface TransactionsRepository {
     suspend fun insertTransaction(transaction: Transaction)
-    fun getAllTransactions(): Flow<List<Transaction>>
     fun getTransaction(id: UUID): Flow<Transaction>
-    fun getTransactions(limit: Int, offset: Int): Flow<List<Transaction>>
+    fun getTransactions(): PagingSource<Int, Transaction>
     fun getBalance(): Flow<Double>
-    fun getTransactionsCount(): Flow<Int>
 }
 
 class LocalTransactionRepository(private val transactionsDao: TransactionsDao) :
@@ -18,15 +17,10 @@ class LocalTransactionRepository(private val transactionsDao: TransactionsDao) :
     override suspend fun insertTransaction(transaction: Transaction) =
         transactionsDao.insert(transaction)
 
-    override fun getAllTransactions(): Flow<List<Transaction>> =
-        transactionsDao.getAllTransactions()
-
     override fun getTransaction(id: UUID): Flow<Transaction> = transactionsDao.getTransaction(id)
 
-    override fun getTransactions(limit: Int, offset: Int): Flow<List<Transaction>> =
-        transactionsDao.getTransactions(limit = limit, offset = offset)
+    override fun getTransactions() = transactionsDao.getTransactions()
 
     override fun getBalance(): Flow<Double> = transactionsDao.getBalance()
-    override fun getTransactionsCount(): Flow<Int> = transactionsDao.getTransactionsCount()
 
 }
